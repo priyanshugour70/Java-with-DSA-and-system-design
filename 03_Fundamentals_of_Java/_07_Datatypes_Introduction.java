6246f8dc354adc6620b6378cfb327d83e6d28125
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-09 10:06:51 +0530

    Datatypes introduction in java

--- 03_Fundamentals_of_Java/_07_Datatypes_Introduction.java
+++ 03_Fundamentals_of_Java/_07_Datatypes_Introduction.java
@@ -3,17 +3,23 @@ public class _07_Datatypes_Introduction {
 
     public static void main(String args[]){
 
+        // Primitive data types :
+            // Numeric DataTypes
+                // Whole Numbers :- 10, 15, 18, 21, 29 ;
+                // Real Nubmers  :- 10.3, 43.2, 34.5, 23.56 ;
 
-        // Numeric DataTypes
-            // Whole Numbers :- 10, 15, 18, 21, 29 ;
-            // Real Nubmers  :- 10.3, 43.2, 34.5, 23.56 ;
+            // Characters DataTypes
+                // char :- 'a', 'A', 'P' ;
 
-        // Characters DataTypes
-            // char :- 'a', 'A', 'P' ;
+            // Logical DataTypes
+                // true - false   ==>  boolean 
 
-        // Logical DataTypes
-            // true - false   ==>  boolean 
+        // Non - Primitive data types :
+            // classes, Strings, Interfaces, and Arrays.
 
-        
+
+
+
+        // we will learn about primitive datatypes :
     }
 }
