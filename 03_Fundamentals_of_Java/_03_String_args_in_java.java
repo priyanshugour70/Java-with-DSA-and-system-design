2952f8279ce624200d16cbf3f6def8c77b47408c
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-09 08:14:57 +0530

    String args in java '

---
+++ 03_Fundamentals_of_Java/_03_String_args_in_java.java
@@ -0,0 +1,17 @@
+public class _03_String_args_in_java {
+    
+
+    public static void main(String args []){
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+    }
+}
+
+
+        // String args plays the role of the Argument which we entered in the program converted in the byted code means in form of .class or other way to say is Compiled file of java .
+
+        // Example :-   javac _03_String_args_in_java
+        //              java _03_String_args_in_java hello world
+
+        // Output :-    hello
+        //              world
