45d301a4b0734ccca665de3fb0449119aa477a5c
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-09 08:13:17 +0530

    Main Method in java

---
+++ 03_Fundamentals_of_Java/_02_Main_Method_in_java.java
@@ -0,0 +1,7 @@
+public class _02_Main_Method_in_java {
+    
+    public static void main(String args []){
+        // The First Program run using Main method and the program name and the class name was same which class name same which hold the main method and main method in inbuild method
+        // public static void main (String args[]){} this is known as main method and main method id the first methond which run firstly in the whole program ..
+    }
+}
