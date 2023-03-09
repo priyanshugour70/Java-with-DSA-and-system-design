35d35ff2c1c8846050bcf756289471baca795815
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-09 08:16:41 +0530

    Variables and its types and Examples in java

---
+++ 03_Fundamentals_of_Java/_05_Variables_examples_in_java.java
@@ -0,0 +1,21 @@
+public class _05_Variables_examples_in_java {
+    
+
+    public static void main(String args[]){
+
+        int Hello = 32 ;
+        
+        int hlo, bye, go ;
+        hlo = 23 ;
+        bye = 54 ;
+        go = 993 ;
+
+        int come = hlo ;
+
+        System.out.println(Hello);
+        System.out.println(hlo);
+        System.out.println(bye);
+        System.out.println(go);
+        System.out.println(come);
+    }
+}
