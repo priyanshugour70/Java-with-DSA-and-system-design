0c09dd0b836e8017fc753bf33d4cf4caddb4c888
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-09 08:09:36 +0530

    How Java Run

---
+++ 03_Fundamentals_of_Java/_00_How_java_run.java
@@ -0,0 +1,12 @@
+public class _00_How_java_run {
+    
+
+    public static void main(String args[]){
+        
+        
+        //          {java Code}     =(Compiler JVM)=>    {java.class file (Byte Code)}     =(run java file )=>      {then we receved mechine understandable code}    =(Mechine understand and gives output)=>  {Output of the code}
+
+        // What is byte code ? ANS: Byte code is the code which is coverted by JVM using compiler and the byte code is portable which is transfered any where thats why java is portable lanugage..
+
+    }
+}
