cc6ddf8cee205d7ee79e0bfb9e543f3bd8c169b6
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-09 08:17:36 +0530

    Some Variables which can start Special Declearation in java

---
+++ 03_Fundamentals_of_Java/_06_Variables_special_declearation_in_java.java
@@ -0,0 +1,15 @@
+public class _06_Variables_special_declearation_in_java {
+    
+
+    public static void main(String args[]){
+
+
+        // only two Special Character use for creating a Variable..
+
+        int _hlo = 2332 ;
+        int $bye = 4343 ;
+        
+        System.out.println(_hlo);
+        System.out.println($bye);
+    }
+}
