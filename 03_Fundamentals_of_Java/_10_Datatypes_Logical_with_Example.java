b70ee75a4e9b80caa9df638694cbacecaa7d9aec
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-09 10:10:53 +0530

    Datatypes Logical with Example in java

---
+++ 03_Fundamentals_of_Java/_10_Datatypes_Logical_with_Example.java
@@ -0,0 +1,18 @@
+public class _10_Datatypes_Logical_with_Example {
+    
+
+    public static void main(String args[]){
+
+        // To manage Locial type of Data ... 
+            // Ex :- true, false ;
+
+        boolean t_logical = true ;
+        boolean f_logical = false ;
+        
+        System.out.println(t_logical);
+        System.out.println(f_logical);
+
+
+        // we use boolean or logical datatype in huge time and the one of the most important datatype boolean ....
+    }
+}
