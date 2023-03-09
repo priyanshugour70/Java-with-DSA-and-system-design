fe969b0ae65f255e1bfbf84b90bff474d6e4c0fc
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-09 08:19:36 +0530

    Datatypes Introduction

---
+++ 03_Fundamentals_of_Java/_07_Datatypes_Introduction.java
@@ -0,0 +1,19 @@
+public class _07_Datatypes_Introduction {
+    
+
+    public static void main(String args[]){
+
+
+        // Numeric DataTypes
+            // Whole Numbers :- 10, 15, 18, 21, 29 ;
+            // Real Nubmers  :- 10.3, 43.2, 34.5, 23.56 ;
+
+        // Characters DataTypes
+            // char :- 'a', 'A', 'P' ;
+
+        // Logical DataTypes
+            // true - false   ==>  boolean 
+
+        
+    }
+}
