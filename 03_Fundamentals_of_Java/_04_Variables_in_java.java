9b6534ceabdc8cd06923bd589c620e719a3e08ab
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-09 08:15:41 +0530

    What is Variables in java

---
+++ 03_Fundamentals_of_Java/_04_Variables_in_java.java
@@ -0,0 +1,26 @@
+public class _04_Variables_in_java{
+
+    public static void main(String args []){
+        
+        // ● A variable is the name of a memory-allocated reserved area. It may be thought of as
+        // the name of a memory location, in other words.
+
+        // ● While the Java programme is running, the value is held in a container.
+
+        // ● To identify the storage location, each variable needs to have a special name.
+
+        // ● A data type is assigned to a variable (we will learn about it after this topic).
+
+        // Type variable_name = value;
+
+        // The name of a variable is variable_name. The variable can be initialised by
+        // supplying an equal sign and a value (initialization i.e. assigning an initial value, is
+        // optional). However, a Java uninitialized local variable is never given a default
+        // value by the compiler.
+        //     
+
+        int hey ;
+        hey = 100 ;
+        System.out.println(hey); // 100
+    }
+}
