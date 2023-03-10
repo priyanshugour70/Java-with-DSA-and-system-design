1d52639f63853739c1671b6f513af4de415bb849
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-10 22:25:55 +0530

    Arithmetic Operators in java

---
+++ 03_Operators_and_Loops/_02_Arithmetic_Operators.java
@@ -0,0 +1,30 @@
+public class _02_Arithmetic_Operators {
+    
+
+    public static void main(String args[]){
+
+
+        // Arithmetic operators are used in mathematical expressions in the same way that they
+        // are used in algebra. The following lists the arithmetic operators:
+        // 1. Addition(+)
+        // 2. Subtraction(-)
+        // 3. Multiplication(*)
+        // 4. Division(/)
+        // 5. Modulus(%)
+
+        int a = 12;
+        int b = 5;
+        int sum = a + b; // addition operator
+        int difference = a - b; // subtraction operator
+        int product = a * b; // multiplication operator
+        int quotient = a / b; // division operator
+        int reminder = a % b ; // Modulus operator
+
+
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(quotient);
+        System.out.println(reminder);
+    }
+}
