8a4428988a0f30a6f68d721781fb7ae5278a1744
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-10 08:43:26 +0530

    Naming Convention for industry sugection

---
+++ 03_Fundamentals_of_Java/_16_Naming_Convention.java
@@ -0,0 +1,23 @@
+public class _16_Naming_Convention {
+    
+
+    public static void main(String args []){
+
+        // All are not compulsery but those all are Sugested
+
+        // Class => Pascalcase : it means every word start Capital Letter.. 
+        
+        // // variable =>  if (single world){
+        //                     then small letter
+        //                 }else if(multipule words){
+        //                     then cameleCase
+        //                 }  
+        
+        // // Methods =>  if (single world){
+        //                     then small letter
+        //                 }else if(multipule words){
+        //                     then cameleCase
+        //                 }  
+        
+    }
+}
