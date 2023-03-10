c80aa90e40501a430ffbf3fd89eb35db7671eab3
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-10 22:26:23 +0530

    Incrementation Operators in java

---
+++ 03_Operators_and_Loops/_03_Incrementation_Operators.java
@@ -0,0 +1,20 @@
+public class _03_Incrementation_Operators {
+    
+
+    public static void main(String args[]){
+        
+        // In Java, the incrementation operator ++ is used to add 1 to a variable's current value. It can be used both as a prefix or postfix operator.
+
+        int count = 0;
+        count++; // post-increment
+        System.out.println(count); // prints 1
+            
+        int i = 5;
+        int j = ++i; // pre-increment
+        System.out.println(i); // prints 6
+        System.out.println(j); // prints 6
+            
+
+
+    }
+}
