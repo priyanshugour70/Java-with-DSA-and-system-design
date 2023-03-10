3630bd3f2c5ce347f7ec9b372c7cdacb1d263fb9
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-10 22:25:33 +0530

    Operators introduction in java

---
+++ 03_Operators_and_Loops/_01_Operators_introduction.java
@@ -0,0 +1,17 @@
+public class _01_Operators_introduction {
+    
+
+    public static void main(String args[]){
+        // In Java, operators are symbols or special characters that perform specific actions on variables or values. They are used to perform arithmetic, comparison, logical, bitwise, and assignment operations.
+
+
+        // Operators in Java can be classified into 6 types:
+        // 1. Arithmetic Operators
+        // 2. Relational Operators
+        // 3. Logical Operators
+        // 4. Assignment Operators
+        // 5. Unary Operators
+        // 6. Bitwise Operators
+    
+    }
+}
