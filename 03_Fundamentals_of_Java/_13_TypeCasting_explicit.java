ec96bf1e05c755616bd8e8a22e515dd4f44ef706
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-10 08:41:38 +0530

    Typecasting explicit in java

---
+++ 03_Fundamentals_of_Java/_13_TypeCasting_explicit.java
@@ -0,0 +1,14 @@
+public class _13_TypeCasting_explicit {
+    
+
+    public static void main(String args []){
+
+
+        // Explicit Typecasting: Explicit typecasting, also known as narrowing, occurs when a data type of higher range is converted into a data type of lower range. Java requires explicit instructions to perform explicit typecasting. For example:
+
+        double num1 = 10.5;
+        int num2 = (int) num1; // Explicit typecasting from double to int
+        System.out.println(num2); // Output: 10
+
+    }
+}
