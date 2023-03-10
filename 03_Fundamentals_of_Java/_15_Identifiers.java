b86f988e3101ad318fe56215c57e2bbbedd2cff0
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-10 08:42:37 +0530

    Identifiers in java

---
+++ 03_Fundamentals_of_Java/_15_Identifiers.java
@@ -0,0 +1,22 @@
+public class _15_Identifiers {
+    
+
+    public static void main(String args){
+        // Identifiers
+        // An identifier is a name given to a package, class, interface, method, or variable. All identifiers
+        // must have different names.
+
+        // In Java, there are a few points to remember while dealing with identifiers :
+        // Rule 1 − All identifiers should begin with a letter (A to Z or a to z), $ and _ and must be
+        // unique.
+        // Rule 2 − After the first character/letter, identifiers can have any combination of characters.
+        // Rule 3 − A keyword cannot be used as an identifier.
+        // Rule 4 − The identifiers are case-sensitive.
+        // Rule 5 – Whitespaces are not permitted.0
+        
+        // Examples of legal identifiers: rank, $name, _rate, __2_mark.
+        // Examples of illegal identifiers: 102pqr, -name.
+
+        // every variable name and or function or other which is use to identify data
+    }
+}
