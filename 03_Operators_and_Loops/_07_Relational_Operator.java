5ec6df1a77177e7030dbe6e2a5b4e5a281546a52
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-11 23:35:25 +0530

    Relational Operator in java

---
+++ 03_Operators_and_Loops/_07_Relational_Operator.java
@@ -0,0 +1,46 @@
+public class _07_Relational_Operator {
+    
+
+    public static void main(String args []){
+
+
+        // In Java, relational operators are used to compare two values and produce a boolean result. Here are the six types of relational operators in Java:
+
+        // == (equal to): This operator checks if two values are equal.
+        int a = 5;
+        int b = 5;
+        boolean result = a == b; // result is true
+        System.out.println(result);
+
+        // != (not equal to): This operator checks if two values are not equal.
+        int c = 5;
+        int d = 7;
+        boolean result1 = c != d; // result is true
+        System.out.println(result1);
+
+        // < (less than): This operator checks if the first value is less than the second value.
+        int e = 5;
+        int f = 7;
+        boolean result2 = e < f; // result is true
+        System.out.println(result2);
+
+        // > (greater than): This operator checks if the first value is greater than the second value.
+        int g = 5;
+        int h = 7;
+        boolean result3 = g > h; // result is true
+        System.out.println(result3);
+
+        // <= (less than or equal to): This operator checks if the first value is less than or equal to the second value.
+        int i = 5;
+        int j = 7;
+        boolean result4 = i <= j; // result is true
+        System.out.println(result4);
+
+        // >= (greater than or equal to): This operator checks if the first value is greater than or equal to the second value.
+        int k = 5;
+        int l = 7;
+        boolean result5 = k >= l; // result is true
+        System.out.println(result5);
+
+    }
+}
