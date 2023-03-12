5d32f79256cfdfbe88d85eb412ceb59647833580
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-12 11:16:13 +0530

    Loop forLoop IN java

---
+++ 03_Operators_and_Loops/_13_Loop_forLoop_In_java.java
@@ -0,0 +1,19 @@
+public class _13_Loop_forLoop_In_java {
+    
+
+    public static void main(String args[]){
+
+
+        // for loop: A for loop is used to iterate over a range of values a fixed number of times. The basic syntax of the for loop is:
+
+        // for(initialization; condition; increment/decrement) {
+        //     // code to be executed
+        // }
+        
+
+        for(int i=1; i<=10; i++) {
+            System.out.println(i);
+        }
+        
+    }
+}
