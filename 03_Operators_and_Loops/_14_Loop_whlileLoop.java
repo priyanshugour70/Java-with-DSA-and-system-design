4be82c4bacdcc20de7ed72584e3dc2067971128a
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-12 11:16:28 +0530

    Loop whileLoop in java

---
+++ 03_Operators_and_Loops/_14_Loop_whlileLoop.java
@@ -0,0 +1,24 @@
+public class _14_Loop_whlileLoop {
+    
+
+    public static void main(String args[]){
+
+
+        // while loop: A while loop is used to repeatedly execute a block of code as long as a certain condition is true. The basic syntax of the while loop is:
+
+
+        // while(condition) {
+        //     // code to be executed
+        // }
+        
+
+
+
+        int i = 1;
+        while(i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
