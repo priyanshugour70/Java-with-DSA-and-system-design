5d90f63f8f3ffcdbeca3c944d8de93c33389aca5
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-12 11:15:49 +0530

    Loops what is loop in java

---
+++ 03_Operators_and_Loops/_12_Loops_what_is_loop.java
@@ -0,0 +1,16 @@
+public class _12_Loops_what_is_loop {
+    
+
+    public static void main(String args[]){
+
+
+        // In programming, a loop is a way to repeatedly execute a block of code until a certain condition is met. There are three main types of loops in Java:
+
+        // for loop
+        // while loop
+        // do-while loop
+
+        // we will discuss sepratly..
+
+    }
+}
