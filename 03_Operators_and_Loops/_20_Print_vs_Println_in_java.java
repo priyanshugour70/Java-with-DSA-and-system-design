01e717b79942205f245511b53f5a458574bfbfda
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-12 11:18:20 +0530

    Print vs Println In java

---
+++ 03_Operators_and_Loops/_20_Print_vs_Println_in_java.java
@@ -0,0 +1,21 @@
+public class _20_Print_vs_Println_in_java {
+    
+
+    public static void main(String args[]){
+
+
+        // print and println are both methods used for displaying output in Java.
+
+        // print displays the output without adding a new line, while println displays the output and adds a new line at the end.
+
+        System.out.print("Hello ");
+        System.out.println("world!");
+        System.out.println("How are you?");
+
+
+        int num = 42;
+        System.out.print("The answer is: " + num);
+        System.out.println("!");
+
+    }
+}
