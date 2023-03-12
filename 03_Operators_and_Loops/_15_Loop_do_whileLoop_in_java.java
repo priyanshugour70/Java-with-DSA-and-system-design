380bbb8274328bdd8dbad847afda6977df4f1355
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-12 11:16:45 +0530

    Loop dowhileLoop in java

---
+++ 03_Operators_and_Loops/_15_Loop_do_whileLoop_in_java.java
@@ -0,0 +1,21 @@
+public class _15_Loop_do_whileLoop_in_java {
+    
+
+    public static void main(String args[]){
+
+        // do-while loop: A do-while loop is similar to a while loop, but the code inside the loop is executed at least once, even if the condition is false. The basic syntax of the do-while loop is:
+
+
+        // do {
+        //     // code to be executed
+        // } while(condition);
+        
+
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        } while(i <= 10);
+
+    }
+}
