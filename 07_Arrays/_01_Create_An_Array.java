1e87ad4a913145911eab3101d88aec6771851e0b
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-17 23:49:56 +0530

    Create An Array in java

---
+++ 07_Arrays/_01_Create_An_Array.java
@@ -0,0 +1,13 @@
+public class _01_Create_An_Array {
+    
+    public static void main(String args[]){
+
+        // In Java, an array is a data structure that stores a fixed-size sequential collection of elements of the same data type. The elements of an array are stored in contiguous memory locations and can be accessed using an index.
+
+        int nums[] = {1,2,3,4,5,6,7,8,9,10};
+
+        System.out.println(nums[0]);
+        System.out.println(nums[1]);
+        System.out.println(nums[2]);
+    }
+}
