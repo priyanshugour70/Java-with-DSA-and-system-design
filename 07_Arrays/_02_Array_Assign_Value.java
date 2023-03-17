fcd1a891917cd8689414d73f64a68585f8f701a5
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-17 23:50:17 +0530

    Array Assign Value in java

---
+++ 07_Arrays/_02_Array_Assign_Value.java
@@ -0,0 +1,19 @@
+public class _02_Array_Assign_Value {
+    
+    public static void main(String[] args) {
+        
+        // Declare an array of integers with size 5
+        int[] numbers = new int[5];
+            
+        // Initialize the array with values
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
+        numbers[3] = 40;
+        numbers[4] = 50;
+            
+        // Access and print the value at index 2
+        System.out.println(numbers[2]); // Output: 30
+
+    }
+}
