1448a1c3bb759550a73e31a8dd5b184ea5b3773b
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-18 22:16:18 +0530

    String Introduction in java

---
+++ 08_Strings/_01_String_Introduction.java
@@ -0,0 +1,12 @@
+public class _01_String_Introduction {
+    
+    public static void main(String args[]){
+
+        // Collection of Character in doublecorts(" Priyanshu ").. 
+        // Stings are Store in Heap and String Packege is (java.lang.string) 
+
+        String myName = "Priyanshu Gour" ;  // This is Known as String ..  
+
+        System.out.println(myName); 
+    }
+}
