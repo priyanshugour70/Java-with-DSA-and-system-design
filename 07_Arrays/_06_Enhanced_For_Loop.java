cae2f1e7b97a294a445f0258a6380b94b2499f1c
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-18 22:11:41 +0530

    Enhaced For loop in java

---
+++ 07_Arrays/_06_Enhanced_For_Loop.java
@@ -0,0 +1,20 @@
+public class _06_Enhanced_For_Loop {
+    
+    public static void main(String args[]){
+
+        // In Java, the enhanced for loop (also known as the "for-each" loop) is a simplified loop construct that makes it easier to iterate through collections or arrays. It was introduced in Java 5.
+
+        // for (type var : collection) {
+        //     loop body
+        // }
+
+
+        int[] numbers = {1, 2, 3, 4, 5};
+        
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+        
+
+    }
+}
