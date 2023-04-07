39b879dc592168bb17e031ce871a3d24d6256f3b
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-04-07 23:03:33 +0530

    Introduction to Polymorphism

---
+++ 09_Oops/_19_Introduction_to_Polymorphism.java
@@ -0,0 +1,28 @@
+// What is polymorphism ?
+// if one thing exists in more than one form then it is called polymorphism. 
+// Polymorphism is a Greek word, where poly means many and morphism means structures or forms.
+
+// 1. Static Polymorphism .
+// 2. Dynamic Polymorphism . 
+
+
+
+// How to achieve polymorphism:
+
+// Polymorphism in Java can be achieved in two ways i.e., method overloading and method overriding.
+// Polymorphism in Java is mainly divided into two types.
+
+// 1. Static Polymorphism:
+// If polymorphism exists at compilation time then it is called Static Polymorphism.
+// Ex: Overloading.
+
+// 2. Dynamic Polymorphism:
+// If the polymorphism exists at runtime then that polymorphism is called Dynamic
+// Polymorphism.
+// Ex: Overriding
+public class _19_Introduction_to_Polymorphism {
+    
+    public static void main(String[] args) {
+
+    }
+}
