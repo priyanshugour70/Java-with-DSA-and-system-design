514f819cac8037572a9fc35b7e8c28d04bf6e129
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-04-07 18:21:29 +0530

    Access Modifiers in java

---
+++ 09_Oops/_16_Access_Modifiers.java
@@ -0,0 +1,22 @@
+public class _16_Access_Modifiers {
+    
+    public static void main(String[] args) {
+        
+
+        System.out.println("Access Modifiers..!");
+
+    //  *****************************************************************************
+    //  * Access    * With Same * Outside Class * Outside Package * Outside Package *
+    //  * Modifiers *   Class   * Same Package  *   SubClass      *   Non-SubClass  *
+    //  *****************************************************************************
+    //  * Public    *    Yes    *      Yes      *       Yes       *        yes      * 
+    //  *****************************************************************************
+    //  * Protected *    Yes    *      Yes      *       Yes       *        No       *
+    //  *****************************************************************************
+    //  * default   *    Yes    *      Yes      *       No        *        No       *
+    //  *****************************************************************************
+    //  * private   *    Yes    *      No       *       No        *        No       *
+    //  *****************************************************************************
+
+    }
+}
