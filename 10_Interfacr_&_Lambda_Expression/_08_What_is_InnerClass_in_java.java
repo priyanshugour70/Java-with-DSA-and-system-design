042bf38dd7302a0a1ac06fd62551784c2fe9373a
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-04-14 06:10:15 +0530

    What is InnerClass in java

---
+++ 10_Interfacr_&_Lambda_Expression/_08_What_is_InnerClass_in_java.java
@@ -0,0 +1,12 @@
+public class _08_What_is_InnerClass_in_java {
+    
+    public static void main(String[] args) {
+        
+        // In Java, an inner class is a class that is defined inside another class. There are four types of inner classes in Java:
+
+        // (1) Non-static or Inner Class
+        // (2) Static Nested Class
+        // (3) Member Class
+        // (4) Anonymous Inner Class
+    }
+}
