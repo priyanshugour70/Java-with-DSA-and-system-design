fba14427b0529d24af6a133a697e88eea3fbeb7b
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-04-15 11:11:55 +0530

    Types of Errors

---
+++ 11_Exception_Handling/_01_Types_of_Errors.java
@@ -0,0 +1,19 @@
+public class _01_Types_of_Errors {
+    
+    public static void main(String[] args) {
+        
+        // Different types of Errors in Java
+        // In any programming language we categorise errors into 3 types
+        // 1. Syntax Error :- If we write wrong syntax.
+        // 2. Logical Error :- Our logic is wrong in the code. 
+        // 3. Runtime Error :- Some Error on the running time of the code. like 5 divided by 0 this code is right but java shows the error.. 
+
+
+        // What is an Exception?
+        // ● An unwanted/expected event that disturbs the normal flow of execution of a program is called "Exception handling".
+        // ● The main objective of Exception handling is to handle the exception.
+        // ● It is available for graceful termination of program.
+
+        
+    }
+}
