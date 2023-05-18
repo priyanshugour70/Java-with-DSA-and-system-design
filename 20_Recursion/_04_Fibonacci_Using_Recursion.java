0b022fa31d9cf8479ef443250fbcb5b9ff2fe818
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-05-18 21:26:28 +0530

    Fibonacci using Recusion

---
+++ 20_Recursion/_04_Fibonacci_Using_Recursion.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+
+public class _04_Fibonacci_Using_Recursion {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the Number to Find Fibonacci : ");
+        int n = sc.nextInt();
+
+        int fibo = Fibonacci(n);
+
+        System.out.println("Fibonacci of Number is : " + fibo);
+
+        sc.close();
+    }
+
+    // Recursive Code to find Fibonacci ..
+    public static int Fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
