9d6aaba55e02bc782351e885b527fd8130993ad6
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-05-20 13:49:21 +0530

    Number Of Stairs

--- 20_Recursion/_06_Finding_Of_Power_Optimized_Approach.java
+++ 20_Recursion/_06_Finding_Of_Power_Optimized_Approach.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
+// Time Complexity : O(log2b)
+// Space Complexity : O(n)
+
 public class _06_Finding_Of_Power_Optimized_Approach {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter the Number to Find Power : ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter the Power of Number : ");
+        int b = sc.nextInt();
+
+        int pw = Power(a, b);
+
+        System.out.println("Power is : " + pw);
+
+        sc.close();
+    }
+
+    // To find Power of an Element using Recusion Optimize Approach
+    public static int Power(int a, int b) {
+        if (b <= 1) {
+            return a;
+        } else {
+            int mid = b / 2;
+            int result = Power(a, mid);
+            int finalResult = result * result;
+
+            if (b % 2 != 0) {
+                return a * finalResult;
+            } else {
+                return finalResult;
+            }
+        }
     }
 }
---
+++ 20_Recursion/_06_Number_Of_Stairs.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+// Time Complexity : O(2^n)
+// Space Complexity : O(n)
+
+public class _06_Number_Of_Stairs {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the Number to Find StairCase : ");
+        int n = sc.nextInt();
+
+        int stair = countStairs(n);
+
+        System.out.println("Number of Stair Types is : " + stair);
+
+        sc.close();
+    }
+
+    // Recursive Code to find Count Stairs ..
+    public static int countStairs(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return countStairs(n - 1) + countStairs(n - 2);
+        }
+    }
+}
