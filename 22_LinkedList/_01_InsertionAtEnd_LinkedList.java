f97df3ad1e1fa8d29fe9332adc6a0e582d8162ee
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-06-02 16:00:49 +0530

    Insertion At End in an LinkedList

---
+++ 22_LinkedList/_01_InsertionAtEnd_LinkedList.java
@@ -0,0 +1,62 @@
+public class _01_InsertionAtEnd_LinkedList {
+
+    Node head;
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    // Implementaion of Insertion of a node at the end..
+    public void insertAtEnd(int newData) {
+        Node newNode = new Node(newData);
+
+        // linkedlist in empty ..
+        if (head == null) {
+            head = new Node(newData);
+            return;
+        }
+
+        // linkedlist is not empty ..
+        newNode.next = null;
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+        return;
+    }
+
+    // Implementaion of Displaying the LinkedList ..
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        _01_InsertionAtEnd_LinkedList list = new _01_InsertionAtEnd_LinkedList();
+
+        list.insertAtEnd(10);
+        list.insertAtEnd(20);
+        list.insertAtEnd(30);
+        list.insertAtEnd(40);
+        list.insertAtEnd(50);
+
+        System.out.println("*********** Print LinkedList ***************");
+        list.display();
+        System.out.println("****** Print LinkedList After Insertion of 60 *******");
+        list.insertAtEnd(60);
+        list.display();
+    }
+}
