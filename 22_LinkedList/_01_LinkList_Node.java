1762b9af3f3c0db589fc438b38f0e4af829674a4
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-06-02 22:01:30 +0530

    LinkList Node Stracture

---
+++ 22_LinkedList/_01_LinkList_Node.java
@@ -0,0 +1,16 @@
+public class _01_LinkList_Node {
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
