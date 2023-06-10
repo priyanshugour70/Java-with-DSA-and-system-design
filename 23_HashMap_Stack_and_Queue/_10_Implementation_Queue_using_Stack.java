1b4e7a06e6b946efe7fc9b28d4c6bd45311e1ff3
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-06-10 17:02:12 +0530

    Implementation of Queue using Stack

--- 23_HashMap_Stack_and_Queue/_10_Implementation_Queue_using_Stack.java
+++ 23_HashMap_Stack_and_Queue/_10_Implementation_Queue_using_Stack.java
@@ -6,51 +6,61 @@
 import java.util.*;
 
 public class _10_Implementation_Queue_using_Stack {
-    Stack<Integer> stack1 = new Stack<>();
-    Stack<Integer> stack2 = new Stack<>();
+    Stack<Integer> s1 = new Stack<Integer>();
+    Stack<Integer> s2 = new Stack<Integer>();
 
-    // insertion of the data in the queue
-    // time complexity : O(1)
-    public void enQueue(int data) {
-        stack1.push(data);
+    public void enQueue(int x) {
+        // Move all elements from s1 to s2
+        while (!s1.isEmpty()) {
+            s2.push(s1.pop());
+        }
+        // Push item into s1
+        s1.push(x);
+
+        // Push everything back to s1
+        while (!s2.isEmpty()) {
+            s1.push(s2.pop());
+        }
     }
 
-    // deletion of the data from the queue - FIFO
+    // Dequeue an item from the queue
     public int deQueue() {
-        int ele;
-        if (stack1.empty() && stack2.empty()) {
-            System.out.println("Queue is empty");
+        // if first stack is empty
+        if (s1.isEmpty()) {
+            System.out.println("Q is Empty.");
             System.exit(0);
         }
-
-        // Push all the elements from stack1 to stack2
-        // time complexity : O(n)
-        if (stack2.empty()) {
-            while (!stack1.empty()) {
-                ele = stack1.pop();
-                stack2.push(ele);
-            }
-        }
-
-        // Pop the top element from stack2
-        // FIFO - First In First Out
-        ele = stack2.pop();
-        return ele;
-
+        // Return top of s1
+        int x = s1.peek();
+        s1.pop();
+        return x;
     }
 
     public static void main(String[] args) {
         _10_Implementation_Queue_using_Stack q = new _10_Implementation_Queue_using_Stack();
 
-        // function calling for insertion in the queue
         q.enQueue(1);
+        System.out.println(q.s1.peek());
+        System.out.println(q.deQueue());
+
         q.enQueue(2);
+        System.out.println(q.s1.peek());
+        System.out.println(q.deQueue());
+
         q.enQueue(3);
+        System.out.println(q.s1.peek());
+        System.out.println(q.deQueue());
+
         q.enQueue(4);
+        System.out.println(q.s1.peek());
+        System.out.println(q.deQueue());
 
-        // function calling for deletion of an element from the queue
-        System.out.println("Deleted element is:" + q.deQueue());
-        System.out.println("Deleted element is:" + q.deQueue());
+        q.enQueue(6);
+        System.out.println(q.s1.peek());
+        System.out.println(q.deQueue());
 
+        q.enQueue(5);
+        System.out.println(q.s1.peek());
+        System.out.println(q.deQueue());
     }
 }
---
+++ 23_HashMap_Stack_and_Queue/_11_Implementation_Stack_using_Queue.java
@@ -0,0 +1,5 @@
+public class _11_Implementation_Stack_using_Queue {
+    public static void main(String[] args) {
+
+    }
+}
