f0cf053003213855e8910ffcceb3d75e4d6fddd3
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-06-23 17:59:31 +0530

    Binary Tree In Order Traversal In java

---
+++ 26_Tree/_04_BTInorderTraversal.java
@@ -0,0 +1,61 @@
+// Data structure to store a binary tree node
+class Node
+{
+    int data;
+    Node left, right;
+ 
+    // Function to create a new binary tree node having a given key
+    public Node(int key)
+    {
+        data = key;
+        left = right = null;
+    }
+}
+ 
+public class _04_BTInorderTraversal
+{
+    // Recursive function to perform inorder traversal on the tree
+    public static void inorder(Node root)
+    {
+        // return if the current node is empty
+        if (root == null) {
+            return;
+        }
+    
+        // Traverse the left subtree
+        inorder(root.left);
+    
+        // Display the data part of the root (or current node)
+        System.out.print(root.data + " ");
+    
+        // Traverse the right subtree
+        inorder(root.right);
+    }
+ 
+    public static void main(String[] args)
+    {
+        /* Construct the following tree
+                   1
+                 /   \
+                /     \
+               2       3
+              /      /   \
+             /      /     \
+            4      5       6
+                  / \
+                 /   \
+                7     8
+        */
+ 
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.right.left = new Node(5);
+        root.right.right = new Node(6);
+        root.right.left.left = new Node(7);
+        root.right.left.right = new Node(8);
+ 
+        inorder(root);
+    }
+}
