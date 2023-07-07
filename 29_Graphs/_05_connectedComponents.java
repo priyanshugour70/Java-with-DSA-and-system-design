2379d396b985542229c00ce01a3bdd5b2c8750d7
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-07-07 22:12:29 +0530

    ConnectedComponents

---
+++ 29_Graphs/_05_connectedComponents.java
@@ -0,0 +1,118 @@
+// In this hole code we are going to find the connected components in a graph.
+
+import java.util.*;
+
+public class _05_connectedComponents {
+
+    static class Edge {
+        int src;
+        int nbr;
+        int wt;
+
+        Edge(int src, int nbr, int wt) {
+            this.src = src;
+            this.nbr = nbr;
+            this.wt = wt;
+        }
+    }
+
+    public static void bfs(ArrayList<Edge> graph[]) {
+        boolean visited[] = new boolean[graph.length];
+        for (int i = 0; i < graph.length; i++) {
+            if (!visited[i]) {
+                bfsUtil(graph, visited);
+            }
+        }
+    }
+
+    public static void bfsUtil(ArrayList<Edge> graph[], boolean visited[]) {
+        Queue<Integer> q = new LinkedList<>();
+
+        q.add(0); // add src to queue
+
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+
+            if (!visited[curr]) { // visit curr
+                System.out.print(curr + " ");
+                visited[curr] = true;
+
+                for (int i = 0; i < graph[curr].size(); i++) {
+                    Edge e = graph[curr].get(i);
+                    q.add(e.nbr);
+                }
+
+            }
+        }
+    }
+
+    public static void dfs(ArrayList<Edge> graph[]) {
+        boolean visited[] = new boolean[graph.length];
+        for (int i = 0; i < graph.length; i++) {
+            if (!visited[i]) {
+                dfsUtil(graph, i, visited);
+            }
+        }
+    }
+
+    public static void dfsUtil(ArrayList<Edge> graph[], int curr, boolean visited[]) {
+        // Visit curr
+        System.out.print(curr + " ");
+
+        // Mark curr as visited
+        visited[curr] = true;
+
+        // Visit all unvisited nbrs
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            if (!visited[e.nbr]) {
+                dfsUtil(graph, e.nbr, visited);
+            }
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge> graph[]) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<Edge>();
+        }
+
+        graph[0].add(new Edge(0, 1, 1));
+        graph[0].add(new Edge(0, 2, 1));
+
+        graph[1].add(new Edge(1, 0, 1));
+        graph[1].add(new Edge(1, 3, 1));
+
+        graph[2].add(new Edge(2, 0, 1));
+        graph[2].add(new Edge(2, 4, 1));
+
+        graph[3].add(new Edge(3, 1, 1));
+        graph[3].add(new Edge(3, 4, 1));
+        graph[3].add(new Edge(3, 5, 1));
+
+        graph[4].add(new Edge(4, 2, 1));
+        graph[4].add(new Edge(4, 3, 1));
+        graph[4].add(new Edge(4, 5, 1));
+
+        graph[5].add(new Edge(5, 3, 1));
+        graph[5].add(new Edge(5, 4, 1));
+        graph[5].add(new Edge(5, 6, 1));
+
+        graph[6].add(new Edge(6, 5, 1));
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+/*
+ * 0 -------- 2
+ * / /
+ * / /
+ * 1 4
+ * \ /
+ * \ /
+ * \ /
+ * 3
+ * FALSE
+ */
