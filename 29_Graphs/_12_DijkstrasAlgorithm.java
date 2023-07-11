e3ef473a09708cb68fe7c710293ca9ddca313763
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-07-11 09:40:57 +0530

    Dijkstras Algorithms

---
+++ 29_Graphs/_12_DijkstrasAlgorithm.java
@@ -0,0 +1,102 @@
+// Dijkstra's Algorithm for Single Source Shortest Path in a Weighted Graph (Adjacency List)
+// we could not use Dijkstra's Algorithm for negative weight cycle(negative weight cycle means sum of all the edges in a cycle is negative)
+// Time Complexity: O(ElogV) (using Priority Queue)
+// Space Complexity: O(V) (for dist array and priority queue)
+
+import java.util.*;
+
+public class _12_DijkstrasAlgorithm {
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int src, int dest, int wt) {
+            this.src = src;
+            this.dest = dest;
+            this.wt = wt;
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge>[] graph) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<Edge>();
+        }
+
+        graph[0].add(new Edge(0, 1, 2));
+        graph[0].add(new Edge(0, 2, 4));
+
+        graph[1].add(new Edge(1, 3, 7));
+        graph[1].add(new Edge(1, 2, 1));
+
+        graph[2].add(new Edge(2, 4, 3));
+
+        graph[3].add(new Edge(3, 5, 1));
+
+        graph[4].add(new Edge(4, 3, 2));
+        graph[4].add(new Edge(4, 5, 5));
+    }
+
+    static class Pair implements Comparable<Pair> {
+        int n;
+        int path;
+
+        public Pair(int n, int path) {
+            this.n = n;
+            this.path = path;
+        }
+
+        @Override
+        public int compareTo(Pair o) {
+            return this.path - o.path;
+        }
+    }
+
+    public static void dijkstraAlgorithm(ArrayList<Edge> graph[], int src) {
+        int dist[] = new int[graph.length];
+
+        for (int i = 0; i < graph.length; i++) {
+            if (i != src) {
+                dist[i] = Integer.MAX_VALUE;
+            }
+        }
+
+        boolean vis[] = new boolean[graph.length];
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        pq.add(new Pair(src, 0));
+
+        while (!pq.isEmpty()) {
+            Pair curr = pq.remove();
+            if (!vis[curr.n]) {
+                vis[curr.n] = true;
+                for (int i = 0; i < graph[curr.n].size(); i++) {
+                    Edge e = graph[curr.n].get(i);
+                    int u = e.src;
+                    int v = e.dest;
+                    int wt = e.wt;
+
+                    if (dist[u] + wt < dist[v]) {
+                        dist[v] = dist[u] + wt;
+                        pq.add(new Pair(v, dist[v]));
+                    }
+                }
+            }
+        }
+
+        // print the dist array
+        for (int i = 0; i < dist.length; i++) {
+            System.out.print(dist[i] + "  ");
+        }
+        System.out.println();
+
+    }
+
+    public static void main(String args[]) {
+        int V = 6;
+        ArrayList<Edge>[] graph = new ArrayList[V];
+        createGraph(graph);
+
+        int src = 0;
+        dijkstraAlgorithm(graph, src);
+    }
+}
